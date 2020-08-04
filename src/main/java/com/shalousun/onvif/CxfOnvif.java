520b91145b52d8238b47cea9487b7705447c5832
oppofind <836575280@qq.com>
2020-08-04 13:48:56 +0800

    添加onvif代码

--- src/main/java/com/shalousun/onvif/CxfOnvif.java
+++ src/main/java/com/shalousun/onvif/CxfOnvif.java
@@ -1,5 +1,13 @@
 package com.shalousun.onvif;
 
+import org.apache.cxf.ws.discovery.WSDiscoveryClient;
+import org.apache.cxf.ws.discovery.wsdl.ProbeMatchType;
+import org.apache.cxf.ws.discovery.wsdl.ProbeMatchesType;
+import org.apache.cxf.ws.discovery.wsdl.ProbeType;
+
+import javax.xml.namespace.QName;
+import java.util.List;
+
 /**
  * @author yu 2020/8/4.
  */
@@ -14,6 +22,7 @@ public class CxfOnvif {
         probeType.getTypes().add(new QName("dn:Network Video Transmitter"));//
         System.out.println("Probe:" + client.getAddress());
         ProbeMatchesType probeMatchesType = client.probe(probeType);
+        //探索设备
         List<ProbeMatchType> probeMatchTypes = probeMatchesType.getProbeMatch();
         for (ProbeMatchType type : probeMatchTypes) {
             System.out.println(type.getXAddrs());
